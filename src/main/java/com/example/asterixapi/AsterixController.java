3d7fac09a45c81917f36037c46a7ae9cac68e632
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-09-15 10:52:13 +0200

    Add new characters (POST)

--- src/main/java/com/example/asterixapi/AsterixController.java
+++ src/main/java/com/example/asterixapi/AsterixController.java
@@ -1,8 +1,6 @@
 package com.example.asterixapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class AsterixController {
     @GetMapping("/characters")
     List<Character> findAllCharacters() {
         return characterRepo.findAll();
+    }
+
+    @PostMapping("/characters")
+    Character addCharacter(@RequestBody Character newCharacter) {
+        return characterRepo.save(newCharacter);
     }
 }
